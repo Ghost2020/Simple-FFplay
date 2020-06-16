577da5b1b980ef139513b09990515bf2e068067f
Ghost2020 <cxx2020@outlook.com>
2020-06-16 15:17:25 +0800

    修复statusPic 未能居中

--- project/pc/XiaoStudio/build/Qt_5_14_0_MSVC2017_64bit-Debug/debug/moc_QSettings.cpp
+++ project/pc/XiaoStudio/build/Qt_5_14_0_MSVC2017_64bit-Debug/debug/moc_QSettings.cpp
@@ -1,19 +1,19 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'QSettings.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.14.2)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.14.0)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
 #include <memory>
-#include "../ui/QSettings.h"
+#include "../../../ui/QSettings.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'QSettings.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.14.2. It"
+#error "This file was generated using the moc from 5.14.0. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
